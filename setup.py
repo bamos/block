5d415a2705977ff3ed4a56b9efe3d4927e0e744f
Brandon Amos <bamos@cs.cmu.edu>
2017-04-11 20:10:03 -0400

    Bump to v0.0.5.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='block',
-    version='0.0.4',
+    version='0.0.5',
     description="Improved block matrix creation for numpy and PyTorch.",
     author='Brandon Amos',
     author_email='bamos@cs.cmu.edu',
